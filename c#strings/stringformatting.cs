870d11146f4ec832c820b64bde1607b5a41c0c6f
amaalselim <141284662+amaalselim@users.noreply.github.com>
2023-08-21 01:34:08 +0300

    Update stringformatting.cs

--- c#strings/stringformatting.cs
+++ c#strings/stringformatting.cs
@@ -11,7 +11,10 @@ namespace strings
             Console.WriteLine(str);
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             double salary = 1000;
-            string s = "Your Salary is : {0}";
+            string s = "Your Salary is : {0:c2}";// print $1000.00
+            string s = "Your Salary is : {0:c5}";// print $1000.00000
+            string s = "Your Salary is : {0:n2}";// print 1000.00
+            string s = "Your Salary is : {0:0.00000}";// print 1000.00000
             s = string.Format(s, salary);
             Console.WriteLine(s);
         }
