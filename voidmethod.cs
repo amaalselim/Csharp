84d63a2a08c13e350a5146353f498061bc3c46d6
amaal selim <you@example.com>
2023-08-20 23:25:41 +0300

    void method to print average to numbersofarray

---
+++ voidmethod.cs
@@ -0,0 +1,22 @@
+﻿namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayofnumbers = { 1, 2, 3, 4, 5 };
+            int[] arrayofnumbers2 = { 6, 100, 28, 5, 39 };
+            calculatavg(arrayofnumbers);
+            calculatavg(arrayofnumbers2);
+        }
+        static void calculatavg(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+             sum += number;
+            double avg = sum / numbers.Length;
+            Console.WriteLine($"Avg of numbers is : {avg}");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+    }
+}
