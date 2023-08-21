5126f5d9d19128e2dc9eabe688bf65bb3e11a49b
amaal selim <you@example.com>
2023-08-21 16:51:20 +0300

    allow user to change console background & foreground

---
+++ changeconsole.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace emuns
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select an Option : ");
+            Console.WriteLine("[1] change Background color   [2] change foreground color");
+            string selectedoption = Console.ReadLine();
+            foreach (var color in Enum.GetNames(typeof(ConsoleColor)))
+            {
+                Console.WriteLine($"{color}");
+            }
+            Console.WriteLine("Please Enter Color Name : ");
+            string colorname= Console.ReadLine();
+            ConsoleColor selectedcolor =(ConsoleColor)Enum.Parse(typeof(ConsoleColor), colorname, true);
+            if (selectedoption == "1")
+                Console.BackgroundColor = selectedcolor;
+            else if (selectedoption=="2")
+                Console.ForegroundColor= selectedcolor;
+            else
+            {
+                Console.WriteLine("Invalid Option!!!!!!!!!!!!");
+            }
+        }
+        
+    }
+}
