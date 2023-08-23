ac77c2315e667ffe9d032b45ddc0755ec0b7bd06
amaalselim <141284662+amaalselim@users.noreply.github.com>
2023-08-24 02:07:28 +0300

    class inside class

---
+++ composition classes.cs
@@ -0,0 +1,35 @@
+﻿namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp = new employee();
+            Console.WriteLine(emp.employeeinsurance.policyname);
+        }
+
+
+    }
+    class employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public insurance employeeinsurance { get; set; }
+        class department
+        {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        }
+        public employee()
+        {
+             employeeinsurance = new insurance { policyid= -1 , policyname= "N/A" };
+        }
+    }
+    
+    class insurance
+    {
+        public int policyid { get; set; }
+        public string policyname { get; set; }
+    }
+
+}
