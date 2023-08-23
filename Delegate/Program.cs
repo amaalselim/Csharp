c6e4e2f284e58a0a5affd60ab0c2912b060bf5f6
amaalselim <141284662+amaalselim@users.noreply.github.com>
2023-08-23 19:31:36 +0300

    Delegates OOP

---
+++ Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emps = new Employee[]
+            {
+                new Employee {Id =1 , Name ="Amaal" , Gender="F",Totalsales=65000m},
+                new Employee {Id =2 , Name ="Ahmed" , Gender="M",Totalsales=50000m},
+                new Employee {Id =3 , Name ="Mohamed" , Gender="M",Totalsales=40000m},
+                new Employee {Id =4 , Name ="Retaj" , Gender="F",Totalsales=30000m},
+                new Employee {Id =5 , Name ="Aya" , Gender="F",Totalsales=16000m},
+                new Employee {Id =6 , Name ="Esraa" , Gender="F",Totalsales=15000m}
+            };
+            var report = new report();
+            report.processemployee(emps, "sales >=$60,0000",s => s.Totalsales > 60000m);
+            report.processemployee(emps, " sales>=$30,000 And sales <=$59,999",s=> s.Totalsales >= 30000m && s.Totalsales < 60000m);
+            report.processemployee(emps, "sales <=$30,0000",s=> s.Totalsales < 30000m);
+            Console.ReadKey();
+        }
+    }
+ }
---
+++ Delegate/report.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegate
+{
+    internal class report
+    {
+        public delegate bool delg(Employee s);
+        public void processemployee(Employee[] employees , string title , delg del)
+        {
+            Console.WriteLine(title);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            foreach (var e in employees)
+            {
+                if (del(e))
+                {
+                    Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | {e.Totalsales}");
+
+                }
+
+            }
+            Console.WriteLine("\n\n");
+        }
+        
+    }
+}
