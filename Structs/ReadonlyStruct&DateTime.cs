4bcab0ed4f221c14865c16432f978f958465f27d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2023-08-24 15:32:39 +0300

    Structs

---
+++ Structs/ReadonlyStruct&DateTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace troubleshooting
+{
+    internal class ReadonlyStructs
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt; // DateTime is Struct (value type);
+            dt = DateTime.Now;
+            Console.WriteLine(dt);
+            dt.AddDays(10); // immutable but when I say : 
+            Console.WriteLine($"immutable: {dt}");
+            dt = dt.AddDays(10); // saved data in dt (changed) so can be mutable;
+            Console.WriteLine($"mutable: {dt}");
+            DateTime dt1 = new DateTime(2023, 08, 24);
+            Console.WriteLine(dt1);
+            DateTime dt2 = new DateTime(2023, 08, 24, 03, 05, 00);
+            Console.WriteLine(dt2);
+
+        }
+
+    }
+    readonly struct Age // When make struck readonly , All of members in struct must be readonly
+    {
+        readonly public int Years;
+        readonly public int whatever;
+        public Age(int years, int whatever)
+        {
+            this.Years = years;
+            this.whatever = whatever;
+        }
+
+    }
+
+}
---
+++ Structs/Structs.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace troubleshooting
+{
+    internal class Structs
+    {
+           
+        static void Main(string[] args)
+        {
+            Digitalsize size = new Digitalsize(60000);
+            size = size.Addbit(8);   // 60000 removed   8 printed;
+            Console.WriteLine(size.Bit); // 8bit
+            Console.WriteLine(size.Byte);// 1 byte
+            Console.WriteLine(size.KB);  // 0 KB
+            Console.WriteLine(size.MB);  //0 MB
+            Console.WriteLine(size.GB);  //0 GB
+            Console.WriteLine(size.TB);  //0 TB
+
+        }
+        
+    }
+    struct Digitalsize
+    {
+        private long bit;
+        private const long bitsinBit = 1;
+        private const long bitsinByte = 8;
+        private const long bitsinKB = bitsinByte*1024;
+        private const long bitsinMB =bitsinKB*1024;
+        private const long bitsinGB =bitsinMB*1024;
+        private const long bitsinTB =bitsinGB*1024;
+        public string Bit => $"{(bit / bitsinBit):N0}Bit";
+        public string Byte => $"{(bit / bitsinByte):N0}Bit";
+        public string KB => $"{(bit / bitsinKB):N0}KB";
+        public string MB => $"{(bit / bitsinMB):N0}MB";
+        public string GB => $"{(bit / bitsinGB):N0}GB";
+        public string TB => $"{(bit / bitsinTB):N0}TB";
+        public Digitalsize(long intialvalue)
+        {
+             this.bit = intialvalue;
+        }
+        private Digitalsize Add(long value , long scale)
+        {
+            return new Digitalsize(value * scale);
+        }
+        public Digitalsize Addbit(long bit)
+        {
+            return Add(bit, bitsinBit);
+        }
+        public Digitalsize Addbyte(long bit)
+        {
+            return Add(bit, bitsinByte);
+        }
+        public Digitalsize AddKB(long bit)
+        {
+            return Add(bit, bitsinKB);
+        }
+        public Digitalsize AddGB(long bit)
+        {
+            return Add(bit, bitsinMB);
+        }
+        public Digitalsize AddMB(long bit)
+        {
+            return Add(bit,bitsinGB);
+        }
+        public Digitalsize AddTB(long bit)
+        {
+            return Add(bit, bitsinTB);
+        }
+    }
+}
