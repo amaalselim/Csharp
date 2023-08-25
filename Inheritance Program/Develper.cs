a85594fdb8f1bab61fb95f03f78e5c1543ab8040
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2023-08-25 22:56:00 +0300

    Add files via upload

---
+++ Inheritance Program/Develper.cs
@@ -0,0 +1,31 @@
+﻿namespace Real_world
+{
+    public class Develper: Employee
+    {
+        private const decimal commission = 0.03m;
+        protected bool Taskcompleted { get; set; }
+        public Develper(int id, string name, decimal loggedhours, decimal wage
+            ,bool Taskcompleted) : base(id, name, loggedhours, wage)
+        {
+            this.Taskcompleted = Taskcompleted;
+        }
+        protected override decimal calculate()
+        {
+            return base.calculate() + calculateBonus();
+        }
+        private decimal calculateBonus()
+        {
+            if(Taskcompleted)
+                return base.calculate()* commission;
+            return 0;
+        }
+        public override string ToString()
+        {
+            return base.ToString() +
+             $"\nTask completed: {(Taskcompleted ? "Yes" : "No")}" +
+             $"\nBonus : ${Math.Round(calculateBonus(), 2):N0}" +
+             $"\nnet salary : ${Math.Round(this.calculate(), 2):N0}";
+        }
+
+    }
+}
