a4185e3b57cd40ae80df0dafd859b2c9f8fd13af
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2023-08-26 03:55:03 +0300

    Interfaces

---
+++ Interfaces/Interfaces.cs
@@ -0,0 +1,86 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            vechile v = new Honda("BMW","new",2022);
+            Iloader v2 = new caterpillor("", "new", 2022);
+            v2.Load(); // output : loading
+            IDrivable v3 = new Honda("BMW", "new", 2022);
+            v3.stop();
+            IDrivable v4 = new caterpillor("BMW", "new", 2022);
+            v4.move();
+            
+            
+            
+        }
+    }
+    abstract class vechile
+    {
+        protected string Brand;
+        protected string model;
+        protected int year;
+
+        public vechile(string brand, string model, int year)
+        {
+            Brand = brand;
+            this.model = model;
+            this.year = year;
+        }
+    }
+    interface Iloader
+    {
+        void Load();
+        void unload();
+    }
+    interface IDrivable
+    {
+        void move();
+        void stop();
+    }
+    class Honda : vechile , IDrivable
+    {
+        public Honda(string brand, string model, int year) :base(brand,model,year)
+        {
+             
+        }
+
+        public void move()
+        {
+            Console.WriteLine("Moving");
+        }
+
+        public void stop()
+        {
+            Console.WriteLine("Stopping");
+        }
+    }
+    class caterpillor : vechile , Iloader , IDrivable
+    {
+        public caterpillor(string brand, string model, int year) : base(brand, model, year)
+        {
+
+        }
+
+        public void Load()
+        {
+            Console.WriteLine("Loading");
+        }
+        public void move()
+        {
+            Console.WriteLine("Moving");
+        }
+
+        public void stop()
+        {
+            Console.WriteLine("Stopping");
+        }
+
+        public void unload()
+        {
+                Console.WriteLine("unLoading");
+        }
+    }
+
+}
---
+++ Interfaces/RealwoldExample.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp7
+{
+    internal class RealwoldExample
+    {
+        static void Main(string[] args)
+        {
+           cash cash = new cash();
+            cash.Pay(9999.99m); // output : Cash Payment : $10,000 
+            Cashier V = new Cashier(new Visa());
+            V.checkout(9999.99m); // output : Cash Payment : $10,000 
+            Cashier D = new Cashier(new Debit());
+            D.checkout(9999.99m); // output : Cash Payment : $10,000
+            Cashier M = new Cashier(new MasterCard());
+            M.checkout(9999.99m); // output : Cash Payment : $10,000
+        }
+    }
+    interface IPayment
+    {
+        void Pay(decimal amount);
+    }
+    class Cashier
+    {
+        private IPayment pay;
+        public Cashier(IPayment payment)
+        {
+             pay = payment;
+        }
+        public void checkout(decimal amount)
+        {
+            pay.Pay(amount);
+        }
+    }
+    class cash : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Cash Payment : ${Math.Round(amount, 2):N0}");
+        }
+    }
+    class Visa : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Cash Payment : ${Math.Round(amount, 2):N0}");
+        }
+    }
+    class Debit : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Cash Payment : ${Math.Round(amount, 2):N0}");
+        }
+    }
+    class MasterCard : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Cash Payment : ${Math.Round(amount, 2):N0}");
+        }
+    }
+
+}
