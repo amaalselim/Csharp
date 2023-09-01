9139b8748d252df0dda407534a30a6dc14579b48
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2023-09-02 02:11:27 +0300

    Generic Delegate

---
+++ GenericDelegate/Action&Func&predicate.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> action = Print; // cannot return value ;
+            action("Amaal");
+            Func<int, int, int> func = Add; // return value ;
+            Console.WriteLine(func(1, 2));
+            Predicate<int> predicate = IsEven; // return value (bool) => true or false;
+            Console.WriteLine(predicate(4));
+        }
+        static void Print(string s) => Console.WriteLine(s);
+        static int Add(int n1 , int n2) => n1 + n2;
+        static bool IsEven(int n) => n % 2 == 0;
+    }
+}
---
+++ GenericDelegate/GenericDelegate.cs
@@ -0,0 +1,26 @@
+﻿namespace k
+{
+    internal class GenericDelegate
+    {
+        public delegate bool filter<in T>(T n);
+        static void Main(string[] args)
+        {
+            IEnumerable<int> list = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            printnumbers(list, n => n <= 6);
+            Console.WriteLine("=============================");
+            printnumbers(list, n => n <= 7);
+            Console.WriteLine("=============================");
+            printnumbers(list, n => n%2==0);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            IEnumerable<decimal> list2 = new decimal[] { 1.0m, 2.55m, 3.34m, 4.32m, 5.55m, 6.66m, 7.344m, 8.98m, 9.10m };
+            printnumbers(list2, n => n > 5.99m);
+        }
+        static void printnumbers<T>(IEnumerable<T> numbers, filter<T> f)
+        {
+            foreach (var n in numbers)
+            {
+                if (f(n)) Console.WriteLine(n);
+            }
+        }
+    }
+}
