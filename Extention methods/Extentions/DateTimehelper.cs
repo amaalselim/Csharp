e14a362b75e81411890c30bda9edf67e968851bf
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2023-09-03 05:35:32 +0300

    Add files via upload

---
+++ Extention methods/Extentions/DateTimehelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public static class DateTimehelper
+    {
+        public static bool IsweekEnd(DateTime value)
+        {
+            return value.DayOfWeek== DayOfWeek.Friday || value.DayOfWeek== DayOfWeek.Saturday;
+        }
+        public static bool IsweekDay(DateTime value)
+        {
+            return !(IsweekEnd(value));
+        }
+
+    }
+}
---
+++ Extention methods/Methodchaning.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp3
+{
+    internal class Methodchaning
+    {
+        static void Main(string[] args)
+        {
+            Pizza p = new Pizza();
+            p.AddDough("thin");
+            p.AddSauce();
+            p.AddCheeze(true);
+            p.AddTopping("black Olives", 3.5m);
+            Console.WriteLine("your pizza : ");
+            Console.WriteLine(p);
+        }
+    }
+    static class PizzaExtentions
+    {
+        public static Pizza AddDough(this Pizza value , string type)
+        {
+            value.Content += $"{type} Dough X $4.00";
+            value.Totalprice += 4m;
+            return value ;  
+        }
+        public static Pizza AddSauce(this Pizza value)
+        {
+            value.Content += $"\nTomato Sauce X $2.00";
+            value.Totalprice += 2m;
+            return value;
+        }
+        public static Pizza AddCheeze(this Pizza value, bool extra)
+        {
+            value.Content += $"\n{(extra?"extra":"regular")} cheeze Sauce X ${(extra ? "6.00" : "4.00")}";
+            value.Totalprice += extra ? 6m : 4m;
+            return value;
+        }
+        public static Pizza AddTopping(this Pizza value, string type,decimal price)
+        {
+            value.Content += $"\n{type} X ${price.ToString("#.##")}";
+            value.Totalprice += price;
+            return value;
+        }
+    }
+    class Pizza
+    {
+        public string Content { get; set; }
+        public  decimal Totalprice { get; set; }
+        public override string ToString()
+        {
+            return $"{Content}\n--------------------------\nTotalprice: ${Totalprice.ToString("#.##")}";
+        }
+    }
+}
