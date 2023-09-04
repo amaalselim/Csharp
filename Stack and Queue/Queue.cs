30b6bf3879a25f0860f92c59e12f54c4c849bad9
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2023-09-04 07:40:42 +0300

    Add files via upload

---
+++ Stack and Queue/Queue.cs
@@ -0,0 +1,42 @@
+﻿namespace Queue
+{
+    internal class Queue
+    {
+        static void Main(string[] args)
+        {
+            Queue<PrintingJob> printingJobs = new Queue<PrintingJob>();
+            printingJobs.Enqueue(new PrintingJob("queue.cs", 2));
+            printingJobs.Enqueue(new PrintingJob("stackoverflow.cs", 5));
+            printingJobs.Enqueue(new PrintingJob("index.html", 3));
+            printingJobs.Enqueue(new PrintingJob("users.pdf", 2));
+            printingJobs.Enqueue(new PrintingJob("documets.xlsx", 3));
+            printingJobs.Enqueue(new PrintingJob("index.css", 5));
+            Console.WriteLine($"Current Queue Count: {printingJobs.Count}");
+            Random rnd = new Random();
+            while (printingJobs.Count > 0)
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                var job = printingJobs.Dequeue();
+                Console.WriteLine($"Printing...[{job}]");
+                System.Threading.Thread.Sleep(rnd.Next(1,5)*1000);
+                Console.WriteLine($"Current Queue Count: {printingJobs.Count}");
+            }
+        }
+        
+    }
+    class PrintingJob
+    {
+        private readonly string _file;
+        private readonly int _copies;
+
+        public PrintingJob(string file, int copies)
+        {
+            _file = file;
+            _copies = copies;
+        }
+        public override string ToString()
+        {
+            return $"{_file} x #{_copies} copies";
+        }
+    }
+}
---
+++ Stack and Queue/Stack.cs
@@ -0,0 +1,78 @@
+﻿using System.Data;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        static void Main(string[] args)
+        {
+            Stack <Command> Undo = new Stack <Command> ();
+            Stack <Command> Redo = new Stack <Command> ();
+            string line;
+            while (true)
+            {
+                Console.Write("url('exit' to Quit): ");
+                line = Console.ReadLine().ToLower();
+                if (line == "exit") break;
+                else if (line == "back")
+                {
+                    if (Undo.Count > 0)
+                    {
+                        var item=Undo.Pop ();
+                        Redo.Push (item);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (line == "forward")
+                {
+                    if (Redo.Count > 0)
+                    {
+                        var item = Redo.Pop();
+                        Undo.Push(item);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    // add url to undo list
+                    Undo.Push(new Command(line));
+                }
+                Console.Clear();
+                Print("back", Undo);
+                Print("Forward", Redo);
+            }
+        }
+        static void Print(string name , Stack<Command> commands)
+        {
+            Console.WriteLine($"{name} history");
+            Console.BackgroundColor = name.ToLower() == "back" ? ConsoleColor.DarkGreen : ConsoleColor.DarkBlue;
+            foreach(var u in commands)
+            {
+                Console.WriteLine($"\t{u}");
+            }
+            Console.BackgroundColor= ConsoleColor.Black;
+                
+        }
+    }
+    class Command
+    {
+        private readonly DateTime _CreatedAt;
+        private readonly string _url;
+
+        public Command(string url)
+        {
+            _CreatedAt = DateTime.Now;
+            _url = url;
+        }
+        public override string ToString()
+        {
+            return $"[{this._CreatedAt.ToString("yyyy-MM-dd")} {this._url}" ;
+        }
+    }
+}
