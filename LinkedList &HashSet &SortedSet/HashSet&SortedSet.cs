2ec14f55dff8df39136a8cc5a2c7a9939db72f09
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2023-09-04 20:26:11 +0300

    Add files via upload

---
+++ LinkedList &HashSet &SortedSet/HashSet&SortedSet.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel;
+
+namespace HashSet
+{
+    internal class HashSet
+    {
+        static void Main(string[] args)
+        {
+            var customers = new List<Customer>
+            {
+               new Customer {name="Amaal S.",telephone="+1 123 123 4567"},
+               new Customer {name="Retaj L.",telephone="+1 123 123 4568"},
+               new Customer {name="Ahmed M.",telephone="+1 123 123 4564"},
+               new Customer {name="Khaled K.",telephone="+1 123 123 4560"},
+               new Customer {name="Hossam A.",telephone="+1 123 123 4563"}
+            };
+            var custhashset1 = new HashSet<Customer>(customers);
+            //custhashset1.Add(new Customer { name = "Amaal S.", telephone = "+1 123 123 4567" }); // ignore this
+
+            var customers2 = new List<Customer>
+            {
+               new Customer {name="Reem A.",telephone="+1 5463 123 4567"},
+               new Customer {name="Aya L.",telephone="+1 1093 123 4568"},
+               new Customer {name=" Hossam A.",telephone="+1 123 123 4563"}
+            };
+            var custhashset2= new HashSet<Customer>(customers2);
+            custhashset1.UnionWith(custhashset2);
+            //custhashset1.IntersectWith(custhashset2);
+            //custhashset1.ExceptWith(custhashset2);
+            Console.WriteLine("Hash Set");
+            Console.WriteLine("-----------------");
+            foreach (var customer in custhashset1)
+            {
+                Console.WriteLine(customer);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sorted Set");
+            Console.WriteLine("-----------------");
+            var custsortedset = new SortedSet<Customer>(customers);
+            custsortedset.Add(new Customer { name = "Baraa K.", telephone = "+1 123 7684 4563" });
+            foreach (var customer in custsortedset)
+            {
+                Console.WriteLine(customer);
+            }
+
+        }
+    }
+    class Customer : IComparable<Customer>
+    {
+        public string name { get; set; }
+        public string telephone { get; set; }
+        public override string ToString()
+        {
+            return $"{name} ({telephone})";
+        }
+        public override int GetHashCode()
+        {
+            var hash = 17;
+            hash= hash * 23 + telephone.GetHashCode();
+            return hash;
+        }
+        public override bool Equals(object obj)
+        {
+            var customer = obj as Customer;
+            if(customer is null) return false;  
+            return this.telephone.Equals(customer.telephone);
+        }
+
+        public int CompareTo(Customer other)
+        {
+            if(object.ReferenceEquals(this, other)) return 0; 
+            if(other is null) return -1;
+            return this.name.CompareTo(other.name);
+        }
+    }
+}
---
+++ LinkedList &HashSet &SortedSet/LinkedList.cs
@@ -0,0 +1,49 @@
+﻿namespace linkedlist
+{
+    internal class LinkedList
+    {
+        static void Main(string[] args)
+        {
+            var lesson1 = new YTVideo { Id = "yv1", Title = "Variables", Duration = new TimeSpan(12, 30, 11) };
+            var lesson2 = new YTVideo { Id = "yv2", Title = "List", Duration = new TimeSpan(11, 30, 33) };
+            var lesson3 = new YTVideo { Id = "yv3", Title = "Dictionary", Duration = new TimeSpan(05, 30, 50) };
+            var lesson4 = new YTVideo { Id = "yv4", Title = "Stack", Duration = new TimeSpan(02, 30, 43) };
+            var lesson5 = new YTVideo { Id = "yv5", Title = "Queue", Duration = new TimeSpan(10, 30, 22) };
+            /* LinkedList<YTVideo> Playlist = new LinkedList<YTVideo>(new YTVideo[]
+             {
+                 lesson1,
+                 lesson2,
+                 lesson3,
+                 lesson4,
+                 lesson5
+             });*/
+            LinkedList<YTVideo> Playlist = new LinkedList<YTVideo>();
+            Playlist.AddFirst(lesson1);
+            Playlist.AddAfter(Playlist.First, lesson2);
+            var node = new LinkedListNode<YTVideo>(lesson3);
+            Playlist.AddAfter(Playlist.First.Next, node);
+            Playlist.AddLast(lesson5);
+            Playlist.AddBefore(Playlist.Last, lesson4);
+            Print("C# from Zero to Hero", Playlist);
+        }
+        static void Print(string title , LinkedList<YTVideo> playlist)
+        {
+            Console.WriteLine($"{title}");
+            foreach(var video in playlist)
+            {
+                Console.WriteLine(video);
+            }
+            Console.WriteLine($"total :{playlist.Count} item(s)");
+        }
+    }
+    class YTVideo
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public TimeSpan Duration { get; set; }
+        public override string ToString()
+        {
+            return $"{Title} ({Duration})\n\thttps;://WWW.Youtube.Com/Watch?V={Id}";
+        }
+    }
+}
